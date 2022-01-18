ea8adc7d6115c8e4238e97d105f7955d0159fcae
slawekvslawek <twitczu4321@gmail.com>
2022-01-18 19:05:28 +0100

    charts error

---
+++ app/business/param.manager.js
@@ -0,0 +1,42 @@
+import paramDAO from '../DAO/paramDAO';
+
+function create() {
+    async function query() {
+        let result = paramDAO.query();
+        if (result) {
+            return result;
+        }
+    }
+
+    async function get(id) {
+      let result = paramDAO.get(id);
+      if (result) {
+          return result;
+      }
+    }
+
+    async function getLast() {
+      let result = paramDAO.getLast();
+      if (result) {
+          return result;
+      }
+    }
+
+    async function get(date) {
+        let result = paramDAO.get(date);
+        if (result) {
+            return result;
+        }
+    }
+
+    return {
+        query: query,
+        get: get,
+        getLast: getLast,
+    };
+}
+
+export default {
+    create: create
+};
+
